02d809e9ad890d2ad75a873e7d268b4769caf3ae
Jeff C <jeff.curie@gmail.com>
2026-03-20 16:00:23 -0700

    feat: Initial project structure from Genesis Protocol

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Project intelligence</h1>
+      <div className="card">
+        <p>
+          Your Vite + React project is ready.
+        </p>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
